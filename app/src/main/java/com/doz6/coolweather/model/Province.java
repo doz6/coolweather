d823dee3daa780708baf8800e692cdd0245b6242
doz6 <doz6com@163.com>
2016-02-10 19:13:55 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/doz6/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.doz6.coolweather.model;
+
+/**
+ * Created by Administrator on 2016/2/10.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
